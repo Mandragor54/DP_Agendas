8a563d4b8520dc257719a27e8f0b635a0b0a42a2
Yvregon <steph.ade17@gmail.com>
2022-12-18 12:56:46 +0100

    Sign up fixed

---
+++ Frontend/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import Navigation from '../components/Navigation.js';
+import MonthlyCalendar from '../components/MonthlyCalendar.js';
+
+const Home = () => {
+
+    return (
+        <div>
+            <Navigation />
+            <div id="content" className ="mt-4" >
+                <h1 className =" text-xl font-bold text-center text-blue-500">PAGE ACCUEIL</h1>
+            </div>
+            <MonthlyCalendar />
+        </div>
+    );
+};
+
+export default Home;
