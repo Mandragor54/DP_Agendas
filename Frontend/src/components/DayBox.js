31f6ba0ce98e130b01c4fa175d0c98f464cd13f1
ParisRomane <romane.piing@gmail.com>
2022-12-18 15:10:49 +0100

    Popup fin, début d'evenement concurant

--- Frontend/src/components/DayBox.js
+++ Frontend/src/components/DayBox.js
@@ -13,17 +13,22 @@ function Day(props) {
   }
   return items
 }
-function addEvent(event,percent){
+function addEvent(event,percent,margin_left){
   const h = (parseInt(event.end.hours)-parseInt(event.start.hours))*10 + (parseInt(event.end.minutes)-parseInt(event.start.minutes))/15*2.5;
   const offset = parseInt(event.start.hours)*10+ parseInt(event.start.minutes)/15*2.5;
-  return <div class="absolute" style={{height: h+'rem',width: percent+'%', 'margin-top': offset+'rem'}}><EventBox description={event.description} start={event.start} end={event.end} color="red"/></div>
+  return <div class="absolute" style={{height: h+'rem',width: percent+'%', 'margin-top': offset+'rem','margin-left': margin_left+'%'}}><EventBox description={event.description} start={event.start} end={event.end} color="red"/></div>
   
+}
+
+function concur(eventA,eventB){
+  //((parseInt(eventA.end.hours)*60+parseInt(eventA.end.minutes)<parseInt(eventB.start.hours)*60+parseInt(eventB.start.minutes)) || (parseInt(eventB.end.hours)*60+parseInt(eventB.end.minutes)-parseInt(eventA.start.hours)*60+parseInt(eventA.start.minutes)))
+  return true
 }
 
 function addEvents(events){
   let items = [];
   for (let indice in events.events) {
-    items.push( addEvent(events.events[indice],100));
+      items.push( addEvent(events.events[indice],100));
   }
   return <div>{items}</div>;
 }
@@ -35,7 +40,7 @@ return (
   {hours()}
   </div>
   <div id="events" class='absolute w-40 opacity-80 ml-8'>
-    {addEvents({events:[{description:"partiel math", start:{hours:'1',minutes:'15'},end:{hours:'2',minutes:'15'}},{description:"partiel info",start:{hours:'4',minutes:'15'},end:{hours:'5',minutes:'15'}}]})}
+    {addEvents({events:[{description:"partiel math", start:{hours:'1',minutes:'15'},end:{hours:'2',minutes:'15'}},{description:"partiel info",start:{hours:'1',minutes:'15'},end:{hours:'5',minutes:'15'}}]})}
   </div>
  </div>
   );
